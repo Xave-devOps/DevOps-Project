1bb2690cb42772614b3036364150bfa90b49564c
Xave <ajxiaoming@gmail.com>
2025-01-12 20:27:42 +0800

    idk

--- public/js/attendance.js
+++ public/js/attendance.js
@@ -62,11 +62,6 @@ function updateStatus(attendanceID, newStatus) {
     .catch((error) => console.error("Error updating attendance:", error));
 }
 
-function refreshAttendance() {
-  // Reloads the page to show updated attendance data
-  location.reload();
-}
-
 function goToHomePage() {
   // Redirects to the homepage (index.html)
   window.location.href = "index.html";
