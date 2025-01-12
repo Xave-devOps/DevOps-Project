3e71dbaa7c71e14df460e32c2cc176add09c6bd5
Xave <ajxiaoming@gmail.com>
2025-01-12 18:53:06 +0800

    save

--- index-test.js
+++ index-test.js
@@ -38,7 +38,7 @@ app.get("/data/db.json", (req, res) => {
 });
 
 // Import and use the create student route
-const createStudentRoute = require("./Util/createStudent.js");
+const createStudentRoute = require("./util/createStudent");
 app.use("/", createStudentRoute);
 
 // Default route to serve the main HTML file
